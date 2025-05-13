26a7caeb2e7cc1fdd06f6f1bb3c62dbf4f9b04e2
Jonas Arter <jonasarter@outlook.com>
2025-05-13 14:38:57 +0200

    added edition

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -1,3 +1,5 @@
+// Create rust playground api crate
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Channel {
     Stable,
@@ -11,28 +13,38 @@ pub enum Mode {
     Debug,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Edition {
+    E2018,
+    E2021,
+    E2024,
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct RunConfig {
+pub struct ExecuteRequest {
     channel: Channel,
     mode: Mode,
+    edition: Edition,
     code: String,
 }
 
-impl RunConfig {
-    pub fn new(channel: Channel, mode: Mode, code: String) -> RunConfig {
-        RunConfig {
+impl ExecuteRequest {
+    pub fn new(channel: Channel, mode: Mode, edition: Edition, code: String) -> ExecuteRequest {
+        ExecuteRequest {
             channel,
             mode,
+            edition,
             code,
         }
     }
 }
 
-pub fn parse_run_command(command: &str, code: &str) -> RunConfig {
+pub fn parse_run_command(command: &str, code: &str) -> ExecuteRequest {
     let parts = command.split_whitespace();
 
     let mut channel = Channel::Stable;
     let mut mode = Mode::Release;
+    let mut edition = Edition::E2024;
 
     for arg in parts {
         match arg.to_lowercase().as_str() {
@@ -41,13 +53,17 @@ pub fn parse_run_command(command: &str, code: &str) -> RunConfig {
             "stable" => channel = Channel::Stable,
             "beta" => channel = Channel::Beta,
             "nightly" => channel = Channel::Nightly,
+            "2018" | "e2018" => edition = Edition::E2018,
+            "2021" | "e2021" => edition = Edition::E2021,
+            "2024" | "e2024" => edition = Edition::E2024,
             _ => {}
         }
     }
 
-    RunConfig {
+    ExecuteRequest {
         channel,
         mode,
+        edition,
         code: code.to_string(),
     }
 }
@@ -59,19 +75,19 @@ mod tests {
     #[test]
     fn parse() {
         let examples = [
-            "/run nightly release",
-            "/run release",
+            "/run nightly release 2018",
+            "/run 2021 release",
             "/run debug beta",
             "/run",
-            "/run unknown stable",
+            "/run unknown e2021 stable",
         ];
 
         let parsed = [
-            RunConfig::new(Channel::Nightly, Mode::Release, "".into()),
-            RunConfig::new(Channel::Stable, Mode::Release, "".into()),
-            RunConfig::new(Channel::Beta, Mode::Debug, "".into()),
-            RunConfig::new(Channel::Stable, Mode::Release, "".into()),
-            RunConfig::new(Channel::Stable, Mode::Release, "".into()),
+            ExecuteRequest::new(Channel::Nightly, Mode::Release, Edition::E2018, "".into()),
+            ExecuteRequest::new(Channel::Stable, Mode::Release, Edition::E2021, "".into()),
+            ExecuteRequest::new(Channel::Beta, Mode::Debug, Edition::E2024, "".into()),
+            ExecuteRequest::new(Channel::Stable, Mode::Release, Edition::E2024, "".into()),
+            ExecuteRequest::new(Channel::Stable, Mode::Release, Edition::E2021, "".into()),
         ];
 
         for i in 0..examples.len() {
