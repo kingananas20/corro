03d082bd2e5ed8b82774a532d2b9a637e9317aa6
Jonas Arter <jonasarter@outlook.com>
2025-05-23 15:46:25 +0200

    added ready

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -12,19 +12,14 @@ pub fn parse_run_command(command: &str, code: String) -> ExecuteRequest {
 
     for arg in parts {
         match arg.to_lowercase().as_str() {
-            "release" => mode = Mode::Release,
-            "debug" => mode = Mode::Debug,
-            "stable" => channel = Channel::Stable,
+            "-r" => mode = Mode::Release,
             "beta" => channel = Channel::Beta,
             "nightly" => channel = Channel::Nightly,
             "2015" | "e2015" => edition = Edition::Edition2015,
             "2018" | "e2018" => edition = Edition::Edition2018,
             "2021" | "e2021" => edition = Edition::Edition2021,
-            "2024" | "e2024" => edition = Edition::Edition2024,
-            "binary" => crate_type = CrateType::Binary,
-            "library" => {
-                crate_type = CrateType::Library(playground_api::endpoints::LibraryType::Cdylib)
-            }
+            "binary" | "bin" => crate_type = CrateType::Binary,
+            "library" | "lib" => crate_type = CrateType::Library,
             "tests" => tests = true,
             "backtrace" => backtrace = true,
             _ => {}
--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,28 @@ mod extract_code;
 
 use dotenv::dotenv;
 use extract_code::extract_code;
+use serenity::all::Ready;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
 use std::env;
 
-const PLAYGROUND_URL: &str = "https://play.rust-lang.org/";
-
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
+    async fn ready(&self, _ctx: Context, ready: Ready) {
+        println!("{} is  connected!", ready.user.display_name());
+    }
+
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content.starts_with("/run") {
+        if msg.content.starts_with("/cargo run") {
             let lines: Vec<_> = msg.content.lines().collect();
 
             let code = extract_code(&msg.content);
 
             let config = commands::run::parse_run_command(lines[0], code);
-            let client = playground_api::Client::new(PLAYGROUND_URL);
+            let client = playground_api::Client::default();
             let res = client.execute(&config).await.unwrap();
 
             if let Err(e) = msg.reply(&ctx.http, format!("```{}```", res.stdout)).await {
