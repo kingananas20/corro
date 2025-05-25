3b674d03b5955e246865a4c9dbb82e9abecd27a6
Jonas Arter <jonasarter@outlook.com>
2025-05-25 20:10:10 +0200

    added versions method and fixed it and made it ready to use

--- src/commands/versions.rs
+++ src/commands/versions.rs
@@ -1,10 +1,51 @@
 use crate::{Context, Error};
-use playground_api::endpoints::Edition;
-use poise::command;
+use playground_api::endpoints::Channel;
+use poise::{
+    CreateReply, command,
+    serenity_prelude::{CreateEmbed, CreateEmbedAuthor},
+};
 
 #[command(slash_command, prefix_command)]
-pub async fn versions(ctx: Context<'_>) -> Result<(), Error> {
+pub async fn versions(ctx: Context<'_>, channel: Channel) -> Result<(), Error> {
     let res = ctx.data().playground_client.versions().await?;
-    ctx.reply(format!("{:?}", res)).await?;
+
+    let (rustc, clippy, rustfmt, miri) = match channel {
+        Channel::Stable => (
+            res.stable.rustc.version,
+            res.stable.clippy.version,
+            res.stable.rustfmt.version,
+            res.stable.miri,
+        ),
+        Channel::Beta => (
+            res.beta.rustc.version,
+            res.beta.clippy.version,
+            res.beta.rustfmt.version,
+            res.beta.miri,
+        ),
+        Channel::Nightly => (
+            res.nightly.rustc.version,
+            res.nightly.clippy.version,
+            res.nightly.rustfmt.version,
+            res.nightly.miri,
+        ),
+    };
+
+    let mut embed = CreateEmbed::new()
+        .color(0xCC5500)
+        .title("Current versions used by the Rust Playground")
+        .field("rustc", rustc, true)
+        .field("rustfmt", rustfmt, true)
+        .field("clippy", clippy, true)
+        .author(
+            CreateEmbedAuthor::new("Cargo").url("https://github.com/kingananas20/playground-bot"),
+        );
+
+    if miri.is_some() {
+        embed = embed.field("miri", miri.unwrap().version, true);
+    }
+
+    let reply = CreateReply::default().embed(embed);
+
+    ctx.send(reply).await?;
     Ok(())
 }
