2aa69947a5df4d4be8bb400b34473059125c08a5
Jonas Arter <jonasarter@outlook.com>
2025-05-31 16:54:35 +0200

    added rust version to embed and made it change colors dependent on the downloads

--- src/commands/krate/info.rs
+++ src/commands/krate/info.rs
@@ -36,9 +36,19 @@ pub async fn info(
 
     let crate_data = crate_info.crate_response.crate_data;
     let latest_version = crate_info.crate_response.versions[0].clone();
+
+    let color = match crate_data.downloads {
+        d if d >= 100_000_000 => 0x2ecc71,
+        d if d >= 1_000_000 => 0x3498db,
+        d if d >= 100_000 => 0xf1c40f,
+        d if d >= 10_000 => 0xe67e22,
+        _ => 0xe74c3c,
+    };
+
     let mut embed = CreateEmbed::new()
         .title(crate_data.name)
-        .timestamp(crate_info.last_updated);
+        .timestamp(crate_info.last_updated)
+        .color(color);
 
     if let Some(homepage) = crate_data.homepage {
         embed = embed.url(homepage);
@@ -64,7 +74,7 @@ pub async fn info(
     embed = embed.field("Version", crate_data.max_version, true);
 
     embed = embed.field(
-        "Last Update",
+        "Last update",
         format!("<t:{}>", crate_data.updated_at.timestamp()),
         true,
     );
@@ -97,14 +107,18 @@ pub async fn info(
                 crate_data.downloads.separate_by_policy(SEPARATOR),
                 recent_downloads.separate_by_policy(SEPARATOR)
             ),
-            false,
+            true,
         );
     } else {
         embed = embed.field(
             "Downloads",
             crate_data.downloads.separate_by_policy(SEPARATOR),
-            false,
+            true,
         );
+    }
+
+    if let Some(rust_version) = latest_version.rust_version {
+        embed = embed.field("Rust version", rust_version, true);
     }
 
     ctx.send(CreateReply::default().embed(embed)).await?;
