5668c23f1dbaa417195a4f687fb2486dea5b44d8
Jonas Arter <jonasarter@outlook.com>
2025-10-09 12:35:51 +0200

    moved the data struct to a different module

---
+++ src/data.rs
@@ -0,0 +1,54 @@
+use crate::cache;
+use docsrs::{Doc, Indexed};
+use log::info;
+
+pub struct Data {
+    pub playground_client: playground_api::Client,
+    pub redis_client: cache::Client,
+    pub crates_io_client: crates_io_api::AsyncClient,
+    pub max_code_size: u32,
+    pub std: Doc<Indexed>,
+    pub core: Doc<Indexed>,
+    pub alloc: Doc<Indexed>,
+}
+
+impl Default for Data {
+    fn default() -> Self {
+        let email = std::env::var("EMAIL").expect("no email specified in the environment");
+
+        info!("reading, parsing and building searchindex for std.json");
+        let std = Doc::from_json("./assets/docs/std.json")
+            .unwrap()
+            .parse()
+            .unwrap()
+            .build_search_index();
+
+        info!("reading, parsing and building searchindex for core.json");
+        let core = Doc::from_json("./assets/docs/core.json")
+            .unwrap()
+            .parse()
+            .unwrap()
+            .build_search_index();
+
+        info!("reading, parsing and building searchindex for alloc.json");
+        let alloc = Doc::from_json("./assets/docs/alloc.json")
+            .unwrap()
+            .parse()
+            .unwrap()
+            .build_search_index();
+
+        Self {
+            playground_client: playground_api::Client::default(),
+            redis_client: cache::Client::default(),
+            crates_io_client: crates_io_api::AsyncClient::new(
+                &format!("corro-discord-bot ({email})"),
+                std::time::Duration::from_millis(1000),
+            )
+            .expect("failed to create an AsyncClient"),
+            max_code_size: 64 * 1024,
+            std,
+            core,
+            alloc,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,20 @@
 mod cache;
 pub mod commands;
 mod common;
+mod data;
 mod error;
 
-use docsrs::Doc;
-use docsrs::Indexed;
+pub use data::Data;
 pub use error::Error;
 pub use error::on_error;
 
 pub type Context<'a> = poise::Context<'a, Data, Error>;
-
-pub struct Data {
-    pub playground_client: playground_api::Client,
-    pub redis_client: cache::Client,
-    pub crates_io_client: crates_io_api::AsyncClient,
-    pub max_code_size: u32,
-    pub std: Doc<Indexed>,
-    pub core: Doc<Indexed>,
-    pub alloc: Doc<Indexed>,
-}
-
-impl Default for Data {
-    fn default() -> Self {
-        let email = std::env::var("EMAIL").expect("no email specified in the environment");
-
-        info!("reading, parsing and building searchindex for std.json");
-        let std = Doc::from_json("./assets/docs/std.json")
-            .unwrap()
-            .parse()
-            .unwrap()
-            .build_search_index();
-
-        info!("reading, parsing and building searchindex for core.json");
-        let core = Doc::from_json("./assets/docs/core.json")
-            .unwrap()
-            .parse()
-            .unwrap()
-            .build_search_index();
-
-        info!("reading, parsing and building searchindex for alloc.json");
-        let alloc = Doc::from_json("./assets/docs/alloc.json")
-            .unwrap()
-            .parse()
-            .unwrap()
-            .build_search_index();
-
-        Self {
-            playground_client: playground_api::Client::default(),
-            redis_client: cache::Client::default(),
-            crates_io_client: crates_io_api::AsyncClient::new(
-                &format!("corro-discord-bot ({email})"),
-                std::time::Duration::from_millis(1000),
-            )
-            .expect("failed to create an AsyncClient"),
-            max_code_size: 64 * 1024,
-            std,
-            core,
-            alloc,
-        }
-    }
-}
 
 // Set up logging
 use chrono::Local;
 use fern::Dispatch;
 use fern::colors::ColoredLevelConfig;
 use log::LevelFilter;
-use log::info;
 
 pub fn setup_logging() -> Result<(), Box<Error>> {
     let colors = ColoredLevelConfig::new()
