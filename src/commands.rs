9f70b1b1202c874790fba1fe53e4b6ba385b4d2c
Jonas Arter <jonasarter@outlook.com>
2025-10-10 16:26:52 +0200

    moved `/crates` and `/version` to be `/cargo` submodules

--- src/commands.rs
+++ src/commands.rs
@@ -1,13 +1,9 @@
 mod cargo;
-mod crates;
 mod docs;
 mod explain;
 mod krate;
-mod version;
 
 pub use cargo::{cargo, run_alias};
-pub use crates::crates;
 pub use docs::docs;
 pub use explain::{explain, reload_errors};
 pub use krate::krate;
-pub use version::version;
--- src/commands/cargo.rs
+++ src/commands/cargo.rs
@@ -1,20 +1,22 @@
-mod publish;
-use publish::publish;
-
-mod run;
-pub use run::run_alias;
-use run::run_code_block;
-
+mod crates;
 mod miri;
-use miri::miri;
+mod publish;
+mod run;
+mod version;
 
 use crate::{Context, Error};
+use crates::crates;
+use miri::miri;
 use poise::command;
+use publish::publish;
+pub use run::run_alias;
+use run::run_code_block;
+use version::version;
 
 #[command(
     prefix_command,
     slash_command,
-    subcommands("run_code_block", "publish", "miri")
+    subcommands("run_code_block", "publish", "miri", "crates", "version")
 )]
 pub async fn cargo(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
